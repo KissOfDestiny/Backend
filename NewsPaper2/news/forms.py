cf7f53a93efd621de70bf9a6c1fa39de00922774
KissOfDestiny <77636637+KissOfDestiny@users.noreply.github.com>
2021-04-13 21:06:57 +0300

    auth

---
+++ NewsPaper2/news/forms.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm, BooleanField
+from .models import Post
+
+
+# Создаём модельную форму
+class PostForm(ModelForm):
+    check_box = BooleanField(label='Ало, Галочка!')  # добавляем галочку, или же true-false поле
+    # в класс мета как обычно надо написать модель по которой будет строится форма и нужные нам поля. Мы уже делали что-то похожее с фильтрами.
+
+    class Meta:
+        model = Post
+        fields = ['author', 'title', 'text', 'categoryType', 'check_box']
---
+++ NewsPaper2/news/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render
+from django.views.generic import ListView, UpdateView, DeleteView, CreateView, DetailView
+from django.core.paginator import Paginator
+from .models import Post
+from .filters import PostFilter
+from .forms import PostForm  # импортируем нашу форму
+from django.contrib.auth.mixins import PermissionRequiredMixin
+
+
+class PostList(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'post'
+    ordering = ['-dateCreation']
+    form_class = PostForm  # добавляем форм класс, чтобы получать доступ к форме через метод POST
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
+        context['form'] = PostForm()
+        return context
+
+
+class PostSearch(ListView):
+    model = Post
+    template_name = 'search.html'
+    context_object_name = 'post'
+    ordering = ['-dateCreation']
+    paginate_by = 1
+    form_class = PostForm  # добавляем форм класс, чтобы получать доступ к форме через метод POST
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
+        context['form'] = PostForm()
+        return context
+
+
+class PostCreateView(PermissionRequiredMixin, CreateView):
+    model = Post
+    template_name = 'add.html'
+    context_object_name = 'post'
+    form_class = PostForm  # добавляем форм класс, чтобы получать доступ к форме через метод POST
+    permission_required = 'news.add_post'
+
+
+class PostEditView(PermissionRequiredMixin, UpdateView):
+    template_name = 'edit.html'
+    form_class = PostForm
+    permission_required = 'news.change_post'
+
+    def get_object(self, **kwargs):
+        id = self.kwargs.get('pk')
+        return Post.objects.get(pk=id)
+
+
+class PostDeleteView(PermissionRequiredMixin, DeleteView):
+    template_name = 'delete.html'
+    queryset = Post.objects.all()
+    success_url = '/news/'
+    permission_required = 'news.delete_post'
+
+
+class PostDetailView(PermissionRequiredMixin, DetailView):
+    template_name = 'detail.html'
+    queryset = Post.objects.all()
+    permission_required = 'news.view_post'
